1d4b0dd428a0ab1c8d0739767b7d9843054b6f3f
xer-yy <nuherenkalcioglu@gmail.com>
2026-03-03 13:35:08 +0300

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,12 +1,20 @@
 from discord.ext import commands
+from database import is_admin
+from config import OWNER_ID
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def check_admin(self, ctx):
+        return ctx.author.id == OWNER_ID or is_admin(ctx.guild.id, ctx.author.id)
+
     @commands.command()
-    async def setadmin(self, ctx):
-        await ctx.send("Admin komutu çalıştı kankam 😎")
+    async def duyuru(self, ctx, *, mesaj):
+        if not self.check_admin(ctx):
+            return await ctx.send("❌ Bu komutu kullanamazsın.")
+
+        await ctx.send(f"📢 DUYURU:\n{mesaj}")
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
