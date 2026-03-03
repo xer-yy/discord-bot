9b57db3440013d6efbcd3de845a36dbe2e7af6eb
xer-yy <nuherenkalcioglu@gmail.com>
2026-03-03 16:54:51 +0300

    Update izm.py

--- izm.py
+++ izm.py
@@ -3,29 +3,30 @@ from discord.ext import commands
 import os
 import asyncio
 
-intents = discord.Intents.default()
+# INTENTS FULL AÇIK
+intents = discord.Intents.all()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} olarak giriş yapıldı!")
     print("Bot aktif!")
 
-
 async def load_cogs():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
-            print(f"{filename} yüklendi.")
-
+            try:
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                print(f"{filename} yüklendi.")
+            except Exception as e:
+                print(f"{filename} yüklenemedi: {e}")
 
 async def main():
     async with bot:
         await load_cogs()
         await bot.start(os.getenv("TOKEN"))
-
 
 asyncio.run(main())
