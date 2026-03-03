885b1cb448385e6930787ebae0415a42c717d06f
xer-yy <nuherenkalcioglu@gmail.com>
2026-03-03 12:39:29 +0300

    Update izm.py

--- izm.py
+++ izm.py
@@ -1,24 +1,31 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} olarak giriş yapıldı!")
     print("Bot aktif!")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
 
-TOKEN = os.getenv("TOKEN")
+async def load_cogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            print(f"{filename} yüklendi.")
 
-if TOKEN is None:
-    print("TOKEN bulunamadı!")
-else:
-    bot.run(TOKEN)
+
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(os.getenv("TOKEN"))
+
+
+asyncio.run(main())
