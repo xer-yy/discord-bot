77ad82b00fbb2593c46cef504c6992275c49969e
xer-yy <nuherenkalcioglu@gmail.com>
2026-03-03 12:43:56 +0300

    Update owner.py

--- cogs /owner.py
+++ cogs /owner.py
@@ -1,17 +1,13 @@
 from discord.ext import commands
-from config import OWNER_IDS
 
 class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def is_owner(ctx):
-        return ctx.author.id in OWNER_IDS
-
     @commands.command()
-    @commands.check(is_owner)
+    @commands.is_owner()
     async def panel(self, ctx):
-        await ctx.send("👑 Owner paneline hoşgeldin.")
+        await ctx.send("Owner panel aktif!")
 
 async def setup(bot):
     await bot.add_cog(Owner(bot))
