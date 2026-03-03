22dce03d5bb6879ca56e301af16c3af084ec2ab7
xer-yy <nuherenkalcioglu@gmail.com>
2026-03-03 14:09:46 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,13 +4,23 @@ conn = sqlite3.connect("bot.db")
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS admins (
+CREATE TABLE IF NOT EXISTS punishments (
     guild_id INTEGER,
-    user_id INTEGER
+    user_id INTEGER,
+    moderator_id INTEGER,
+    type TEXT,
+    reason TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 )
 """)
-
 conn.commit()
+
+def add_punishment(guild_id, user_id, moderator_id, ptype, reason):
+    cursor.execute(
+        "INSERT INTO punishments (guild_id, user_id, moderator_id, type, reason) VALUES (?, ?, ?, ?, ?)",
+        (guild_id, user_id, moderator_id, ptype, reason)
+    )
+    conn.commit()
 
 def add_admin(guild_id, user_id):
     cursor.execute("INSERT INTO admins VALUES (?, ?)", (guild_id, user_id))
