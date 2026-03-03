4d489a745a6d44cf627b8e6f0270351adc6768ba
xer-yy <nuherenkalcioglu@gmail.com>
2026-03-03 13:58:42 +0300

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from database import is_admin
 from config import OWNER_ID
@@ -12,9 +13,28 @@ class Admin(commands.Cog):
     @commands.command()
     async def duyuru(self, ctx, *, mesaj):
         if not self.check_admin(ctx):
-            return await ctx.send("❌ Bu komutu kullanamazsın.")
+            await ctx.send("❌ Bu komutu kullanamazsın.")
+            return
 
-        await ctx.send(f"📢 DUYURU:\n{mesaj}")
+        kanal = discord.utils.get(ctx.guild.text_channels, name="duyurular")
+
+        if kanal is None:
+            await ctx.send("❌ 'duyurular' adında kanal bulunamadı.")
+            return
+
+        embed = discord.Embed(
+            title="📢 DUYURU",
+            description=f"# {mesaj}",
+            color=discord.Color.red()
+        )
+
+        embed.set_author(
+            name=ctx.author.display_name,
+            icon_url=ctx.author.display_avatar.url
+        )
+
+        await kanal.send(embed=embed)
+        await ctx.send("✅ Duyuru gönderildi.")
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
